89b92d34a5beea8fd0297212f52de714f7d38474
Sebastian Tleye <stleye@gmail.com>
2024-03-26 13:34:42 -0300

    Added ColorCounter and SquareScreen

---
+++ rn-starter/src/components/ColorCounter.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { View, Text, Button } from "react-native";
+
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+    return (
+        <View>
+            <Text>{color}</Text>
+            <Button title={`Increase ${color}`} onPress={ () => onIncrease() } />
+            <Button title={`Decrease ${color}`} onPress={ () => onDecrease() }  />
+        </View>
+    )
+}
+
+export default ColorCounter;
